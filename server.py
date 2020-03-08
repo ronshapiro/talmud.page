f18de4a41ae1d92b946cc962bf74a115875f96cf
Ron Shapiro <shapiro.rd@gmail.com>
2020-03-08 17:17:48 +0200

    Remove sefaria data from master. It's backed up in sefaria-data-cloned

--- server.py
+++ server.py
@@ -10,7 +10,6 @@ from flask import request
 from flask import send_file
 from flask import url_for
 from link_sanitizer import sanitize_sefaria_links
-from tanach import Tanach
 import datetime
 import json
 import os
@@ -23,7 +22,6 @@ import uuid
 random_hash = ''.join(random.choice(string.ascii_letters) for i in range(7))
 app = Flask(__name__)
 books = Books()
-tanach = Tanach()
 
 def _read_json_file(path):
     with open(path, "r") as f:
@@ -305,49 +303,6 @@ def _get_comments_at_label_indices(source, label_indices):
         if i + 1 in label_indices:
             result.append(comment)
     return result
-
-def _amud_json(masechet, amud):
-    if BIBLICAL_INDEX is None:
-        BIBLICAL_INDEX = _read_json_file("sefaria-data/gemara-biblical-links.json")
-        COMMENTARY_INDEX = _read_json_file("sefaria-data/gemara-commentary-links.json")
-    gemara = books.gemara(masechet)[amud]
-    english = books.gemara_english(masechet)[amud]
-    rashi = books.rashi(masechet)[amud]
-    tosafot = books.tosafot(masechet)[amud]
-    rashba = books.rashba(masechet)[amud]
-    ramban = books.ramban(masechet)[amud]
-
-    sections = []
-    for i in range(len(gemara)):
-        label = "%s:%s" %(amud, i + 1)
-        commentary_index = COMMENTARY_INDEX[masechet].get(label, {})
-        biblical_index = BIBLICAL_INDEX[masechet].get(label, {})
-        sections.append({
-            "gemara": gemara[i],
-            # English is missing when the Hadran is at the end of the Amud, e.g. Brachot 34b
-            "english": english[i] if i < len(english) else [],
-            "rashi": rashi[i] if i < len(rashi) else [],
-            "tosafot": tosafot[i] if i < len(tosafot) else [],
-            "rashba": _get_comments_at_label_indices(rashba, commentary_index.get("rashba", [])),
-            "ramban": _get_comments_at_label_indices(ramban, commentary_index.get("ramban", [])),
-            "quotedVerses": _get_verse_texts(biblical_index)
-        })
-
-    return dict(masechet=masechet,
-                amud=amud,
-                sections=sections)
-
-def _get_verse_texts(verses):
-    return [
-        {"hebrew": tanach.hebrew(verse["book"])[verse["chapter"]][verse["verse"]],
-         "english": tanach.english(verse["book"])[verse["chapter"]][verse["verse"]],
-         "label": {
-             "hebrew": "%s %s:%s" %(verse["book"], verse["chapter"] + 1, verse["verse"]),
-             "english": "%s %s:%s" %(verse["book"], verse["chapter"] + 1, verse["verse"]),
-         }
-        }
-        for verse in verses
-    ]
 
 @app.route("/preferences")
 def preferences():
