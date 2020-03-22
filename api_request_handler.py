e3e53a71654b481708f0a616d29a5aea087263fe
Ron Shapiro <shapiro.rd@gmail.com>
2020-03-22 13:20:23 +0200

    Extract ApiRequestHandler into it's own file

---
+++ api_request_handler.py
@@ -0,0 +1,178 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from flask import jsonify
+from link_sanitizer import sanitize_sefaria_links
+import re
+import requests
+
+HADRAN_PATTERN = re.compile("^(<br>)+<big><strong>הדרן עלך .*")
+BR_PREFIX = re.compile("^(<br>)+")
+
+class ApiRequestHandler(object):
+    def __init__(self, request_maker):
+        self._request_maker = request_maker
+
+    def amud_api_request(self, masechet, amud):
+        sefaria_result = requests.get(
+            # https://github.com/Sefaria/Sefaria-Project/wiki/API-Documentation
+            "https://sefaria.org/api/texts/{masechet}.{amud}".format(masechet=masechet, amud=amud),
+            # "context", "pad", and "multiple" are specified by Sefaria, but it's unclear what they do
+            params = {
+                "commentary": "1",
+                # Even with wrapLinks=1, Jastrow (and perhaps more) is still wrapped. Instead, an active
+                # filtering is performed just in case.
+                "wrapLinks": "0",
+            })
+        if sefaria_result.status_code is not 200:
+            return sefaria_result.text, 500
+        try:
+            sefaria_json = sefaria_result.json()
+        except:
+            return sefaria_result.text, 500
+
+        result = {"id": amud}
+
+        for i in ["title"]:
+            result[i] = sefaria_json[i]
+
+        hebrew = sefaria_json["he"]
+        english = sefaria_json["text"]
+
+        if len(hebrew) != len(english):
+            return "Hebrew length != English length: %s" %(sefaria_result.text), 500
+
+        result["sections"] = []
+        for i in range(len(hebrew)):
+            result["sections"].append({
+                "he": hebrew[i],
+                "en": sanitize_sefaria_links(english[i]),
+                "commentary": {},
+                })
+
+        section_prefix = "%s %s:" %(sefaria_json["book"], amud)
+        for comment in sefaria_json["commentary"]:
+            if len(comment["he"]) is 0 and \
+               len(comment["text"]) is 0:
+                continue
+
+            # TODO: question: if this spans multiple sections, is placing it in the first always correct?
+            section = int(comment["anchorRefExpanded"][0][len(section_prefix):]) - 1
+
+            if section >= len(result["sections"]):
+                print("Unplaceable comment:", comment["sourceRef"], comment["anchorRefExpanded"])
+                continue
+
+            commentary_dict = result["sections"][section]["commentary"]
+            matching_commentary_kind = _matching_commentary_kind(comment)
+            if not matching_commentary_kind:
+                continue
+
+            if matching_commentary_kind["englishName"] not in commentary_dict:
+                commentary_dict[matching_commentary_kind["englishName"]] = []
+
+            comment_english = sanitize_sefaria_links(comment["text"])
+            if comment["he"] == comment_english:
+                # Fix an issue where sometimes Sefaria returns the exact same text. For now, safe to
+                # assume that the equivalent text is Hebrew
+                comment_english = ""
+
+            commentary_dict[matching_commentary_kind["englishName"]].append({
+                "he": comment["he"],
+                "en": comment_english,
+                "sourceRef": comment["sourceRef"],
+                "sourceHeRef": comment["sourceHeRef"],
+                })
+
+        last_section = result["sections"][len(result["sections"]) - 1]
+        if HADRAN_PATTERN.findall(last_section["he"]):
+            last_section["he"] = BR_PREFIX.sub("<br>", last_section["he"])
+            last_section["en"] = ""
+            last_section["commentary"] = {}
+            last_section["hadran"] = True
+
+        return jsonify(result)
+
+_COMMENTARIES = [
+    {
+        "englishName": "Translation",
+    },
+    {
+        "englishName": "Verses",
+        "category": "Tanakh",
+    },
+    {
+        "englishName": "Mishnah",
+        "category": "Mishnah",
+    },
+    {
+        "englishName": "Tosefta",
+        "englishNamePattern": re.compile("^Tosefta "),
+    },
+    {
+        "englishName": "Rashi",
+    },
+    {
+        "englishName": "Tosafot",
+    },
+    {
+        "englishName": "Ramban",
+    },
+    {
+        "englishName": "Rashba",
+    },
+    {
+        "englishName": "Maharsha",
+        "englishNamePattern": re.compile("(Chidushei Halachot|Chidushei Agadot)"),
+    },
+    {
+        "englishName": "Maharshal",
+        "englishNamePattern": re.compile("(Chokhmat Shlomo on .*|Chokhmat Shlomo)"),
+    },
+    {
+        "englishName": "Rosh",
+        "englishNamePattern": re.compile("^Rosh on "),
+    },
+    {
+        "englishName": "Ritva",
+    },
+    {
+        "englishName": "Rav Nissim Gaon",
+        "englishNamePattern": re.compile("^Rav Nissim Gaon on "),
+    },
+    {
+        "englishName": "Shulchan Arukh",
+        "englishNamePattern": re.compile("^Shulchan Arukh, "),
+    },
+    {
+        "englishName": "Mishneh Torah",
+        "englishNamePattern": re.compile("^Mishneh Torah, "),
+    },
+    #  {
+    #    "englishName": "Sefer Mitzvot Gadol",
+    #  },
+    {
+        "englishName": "Mesorat Hashas",
+        "type": "mesorat hashas",
+    },
+    {
+        "englishName": "Jastrow",
+    },
+    {
+        "englishName": "Steinsaltz",
+    }
+];
+
+def _has_matching_property(first, second, property_name):
+    return property_name in first and \
+        property_name in second and \
+        first[property_name] == second[property_name]
+
+def _matching_commentary_kind(comment):
+    name = comment["collectiveTitle"]["en"]
+    for kind in _COMMENTARIES:
+        if name == kind["englishName"] or \
+           _has_matching_property(comment, kind, "category") or \
+           _has_matching_property(comment, kind, "type") or \
+           "englishNamePattern" in kind and kind["englishNamePattern"].findall(name):
+            return kind
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from api_request_handler import ApiRequestHandler
 from flask import Flask
 from flask import jsonify
 from flask import redirect
@@ -8,23 +9,14 @@ from flask import render_template as flask_render_template
 from flask import request
 from flask import send_file
 from flask import url_for
-from link_sanitizer import sanitize_sefaria_links
 from masechtot import Masechtot
-import datetime
-import json
 import os
 import random
-import re
-import requests
 import string
-import uuid
 
 random_hash = ''.join(random.choice(string.ascii_letters) for i in range(7))
 app = Flask(__name__)
 masechtot = Masechtot()
-
-HADRAN_PATTERN = re.compile("^(<br>)+<big><strong>הדרן עלך .*")
-BR_PREFIX = re.compile("^(<br>)+")
 
 def render_template(template_name, **extra_template_variables):
     return flask_render_template(
@@ -105,167 +97,7 @@ def page_not_found(e):
 # TODO: cache this
 @app.route("/api/<masechet>/<amud>")
 def amud_json(masechet, amud):
-    sefaria_result = requests.get(
-        "https://sefaria.org/api/texts/{masechet}.{amud}".format(masechet=masechet, amud=amud),
-        # "context", "pad", and "multiple" are specified by Sefaria, but it's unclear what they do
-        params = {
-            "commentary": "1",
-            # Even with wrapLinks=1, Jastrow (and perhaps more) is still wrapped. Instead, an active
-            # filtering is performed just in case.
-            "wrapLinks": "0",
-        })
-    if sefaria_result.status_code is not 200:
-        return sefaria_result.text, 500
-    try:
-        sefaria_json = sefaria_result.json()
-    except:
-        return sefaria_result.text, 500
-
-    result = {"id": amud}
-
-    for i in ["title"]:
-        result[i] = sefaria_json[i]
-
-    hebrew = sefaria_json["he"]
-    english = sefaria_json["text"]
-
-    if len(hebrew) != len(english):
-        return "Hebrew length != English length: %s" %(sefaria_result.text), 500
-
-    result["sections"] = []
-    for i in range(len(hebrew)):
-        result["sections"].append({
-            "he": hebrew[i],
-            "en": sanitize_sefaria_links(english[i]),
-            "commentary": {},
-            })
-
-    section_prefix = "%s %s:" %(sefaria_json["book"], amud)
-    for comment in sefaria_json["commentary"]:
-        if len(comment["he"]) is 0 and \
-           len(comment["text"]) is 0:
-            continue
-
-        # TODO: question: if this spans multiple sections, is placing it in the first always correct?
-        section = int(comment["anchorRefExpanded"][0][len(section_prefix):]) - 1
-
-        if section >= len(result["sections"]):
-            print("Unplaceable comment:", comment["sourceRef"], comment["anchorRefExpanded"])
-            continue
-
-        commentary_dict = result["sections"][section]["commentary"]
-        matching_commentary_kind = _matching_commentary_kind(comment)
-        if not matching_commentary_kind:
-            continue
-
-        if matching_commentary_kind["englishName"] not in commentary_dict:
-            commentary_dict[matching_commentary_kind["englishName"]] = []
-
-        comment_english = sanitize_sefaria_links(comment["text"])
-        if comment["he"] == comment_english:
-            # Fix an issue where sometimes Sefaria returns the exact same text. For now, safe to
-            # assume that the equivalent text is Hebrew
-            comment_english = ""
-
-        commentary_dict[matching_commentary_kind["englishName"]].append({
-            "he": comment["he"],
-            "en": comment_english,
-            "sourceRef": comment["sourceRef"],
-            "sourceHeRef": comment["sourceHeRef"],
-            })
-
-    last_section = result["sections"][len(result["sections"]) - 1]
-    if HADRAN_PATTERN.findall(last_section["he"]):
-        last_section["he"] = BR_PREFIX.sub("<br>", last_section["he"])
-        last_section["en"] = ""
-        last_section["commentary"] = {}
-        last_section["hadran"] = True
-
-    return jsonify(result)
-
-def has_matching_property(first, second, property_name):
-    return property_name in first and \
-        property_name in second and \
-        first[property_name] == second[property_name]
-
-_COMMENTARIES = [
-    {
-        "englishName": "Translation",
-    },
-    {
-        "englishName": "Verses",
-        "category": "Tanakh",
-    },
-    {
-        "englishName": "Mishnah",
-        "category": "Mishnah",
-    },
-    {
-        "englishName": "Tosefta",
-        "englishNamePattern": re.compile("^Tosefta "),
-    },
-    {
-        "englishName": "Rashi",
-    },
-    {
-        "englishName": "Tosafot",
-    },
-    {
-        "englishName": "Ramban",
-    },
-    {
-        "englishName": "Rashba",
-    },
-    {
-        "englishName": "Maharsha",
-        "englishNamePattern": re.compile("(Chidushei Halachot|Chidushei Agadot)"),
-    },
-    {
-        "englishName": "Maharshal",
-        "englishNamePattern": re.compile("(Chokhmat Shlomo on .*|Chokhmat Shlomo)"),
-    },
-    {
-        "englishName": "Rosh",
-        "englishNamePattern": re.compile("^Rosh on "),
-    },
-    {
-        "englishName": "Ritva",
-    },
-    {
-        "englishName": "Rav Nissim Gaon",
-        "englishNamePattern": re.compile("^Rav Nissim Gaon on "),
-    },
-    {
-        "englishName": "Shulchan Arukh",
-        "englishNamePattern": re.compile("^Shulchan Arukh, "),
-    },
-    {
-        "englishName": "Mishneh Torah",
-        "englishNamePattern": re.compile("^Mishneh Torah, "),
-    },
-    #  {
-    #    "englishName": "Sefer Mitzvot Gadol",
-    #  },
-    {
-        "englishName": "Mesorat Hashas",
-        "type": "mesorat hashas",
-    },
-    {
-        "englishName": "Jastrow",
-    },
-    {
-        "englishName": "Steinsaltz",
-    }
-];
-
-def _matching_commentary_kind(comment):
-    name = comment["collectiveTitle"]["en"]
-    for kind in _COMMENTARIES:
-        if name == kind["englishName"] or \
-           has_matching_property(comment, kind, "category") or \
-           has_matching_property(comment, kind, "type") or \
-           "englishNamePattern" in kind and kind["englishNamePattern"].findall(name):
-            return kind
+    return ApiRequestHandler(None).amud_api_request(masechet, amud)
 
 @app.route("/preferences")
 def preferences():
