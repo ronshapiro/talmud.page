1f1f26e0f56e198e1c1b5c742d79b0504f0c1191
Ron Shapiro <shapiro.rd@gmail.com>
2020-03-22 23:23:50 +0200

    Add pre-commit for checking python tests

---
+++ pre-commit/check_python_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#set -eu
+
+. venv/bin/activate
+
+for test_file in $(find * | grep test.py\$ | grep -v ^venv); do
+    output=$(python $test_file 2>&1)
+    if (( $? )); then
+        echo $test_file failed:
+        echo "$output"
+        exit 1
+    fi
+done
