cb4cc7500a11ec80c251ff59153aae8aabf6b0e1
Ron Shapiro <shapiro.rd@gmail.com>
2020-07-22 19:55:16 +0300

    Extract ref selection snackbar into it's own file

--- js/talmud_page.js
+++ js/talmud_page.js
@@ -7,6 +7,7 @@ import {onceDocumentReady} from "./once_document_ready.js";
 import MASECHTOT from "./masechtot.js";
 import {amudMetadata, computePreviousAmud, computeNextAmud} from "./amud.js";
 import {driveClient} from "./google_drive.js";
+import {registerRefSelectionSnackbarListener} from "./ref_selection_snackbar.js";
 
 // TODO: reactify?
 
@@ -169,132 +170,10 @@ const main = () => {
   $("#next-amud-container").click(addNextAmud);
 };
 
-let selectionSnackbarRef;
-const hideSelectionChangeSnackbar = (ref) => {
-  if (selectionSnackbarRef) {
-    gtag("event", "selection_change_snackbar.hidden", {ref});
-    selectionSnackbarRef = undefined;
-    snackbars.textSelection.hide();
-  }
-};
-
-const findSefariaRef = (node) => {
-  let isEnglish = false;
-  while (node.parentElement) {
-    const $parentElement = $(node.parentElement);
-    isEnglish = isEnglish || $parentElement.hasClass("english");
-    const isTranslationOfSourceText = $parentElement.attr("commentary-kind") === "Translation";
-    const ref = $parentElement.attr("sefaria-ref");
-    if (ref === "ignore") {
-      break;
-    }
-    if (ref && ref !== "synthetic") {
-      if (isEnglish && isTranslationOfSourceText) {
-        // Go up one layer to the main text
-        isEnglish = false;
-      } else {
-        return {
-          ref,
-          parentRef: $parentElement.parent().closest("[sefaria-ref]").attr("sefaria-ref"),
-          text: $($parentElement.find(".hebrew")[0]).text(),
-          translation: isTranslationOfSourceText
-            ? undefined
-            : $($parentElement.find(".english")[0]).text(),
-          amud: $parentElement.closest(".amudContainer").attr("amud"),
-        };
-      }
-    }
-    node = node.parentNode;
-  }
-  return {};
-};
-
-document.addEventListener("selectionchange", () => {
-  let selection = document.getSelection();
-  if (selection.type !== "Range") {
-    hideSelectionChangeSnackbar();
-    return;
-  }
-  const sefariaRef = findSefariaRef(selection.anchorNode);
-  if (!sefariaRef.ref
-      // TODO: perhaps support multiple refs, and just grab everything in between?
-      // If the selection spans multiple refs, ignore them all
-      || sefariaRef.ref !== findSefariaRef(selection.focusNode).ref) {
-    hideSelectionChangeSnackbar(sefariaRef.ref);
-    return;
-  }
-  if (sefariaRef.ref === selectionSnackbarRef) {
-    // do nothing, since the snackbar is already showing the right information
-    return;
-  }
-
-  const {ref} = sefariaRef;
-  const sefariaUrl = `https://www.sefaria.org/${ref.replace(/ /g, "_")}`;
-  gtag("event", "selection_change_snackbar.shown", {ref});
-  selectionSnackbarRef = ref;
-  // don't allow buttons to refer to the selection that triggered the snackbar, since the selection
-  // may have changed
-  selection = undefined;
-  const buttons = [
-    {
-      text: "View on Sefaria",
-      onClick: () => {
-        window.location = sefariaUrl;
-        gtag("event", "view_on_sefaria", {ref});
-      },
-    },
-    {
-      text: "Report correction",
-      onClick: () => {
-        gtag("event", "report_correction", {ref});
-        const subject = "Sefaria Text Correction from talmud.page";
-        let body = [
-          `${ref} (${sefariaUrl})`,
-          sefariaRef.text,
-        ];
-        if (sefariaRef.translation && sefariaRef.translation !== "") {
-          body.push(sefariaRef.translation);
-        }
-        // trailing newline so that the description starts on its own line
-        body.push("Describe the error:\n");
-
-        body = body.join("\n\n");
-        // TODO: verify the iOS versions. Also verify what non-Gmail clients do
-        if (/(Android|iPhone|iPad|iOS)/.test(navigator.userAgent)) {
-          body = body.replace(/\n/g, "<br>");
-        }
-        body = encodeURIComponent(body);
-        window.open(`mailto:corrections@sefaria.org?subject=${subject}&body=${body}`);
-      },
-    },
-  ];
-  if (driveClient.isSignedIn && !driveClient.errors.length) {
-    buttons.push({
-      text: "Add Note",
-      onClick: () => {
-        const modalContainer = $("#modal-container");
-        const noteTextArea = $("#personal-note-entry");
-        modalContainer.show();
-        $("#modal-label").text(`Add a note on ${sefariaRef.ref}`);
-        // Get the possibly-updated selection
-        noteTextArea.val(document.getSelection().toString());
-        $("#modal-cancel").off("click").on("click", () => modalContainer.hide());
-        $("#modal-save").off("click").on("click", () => {
-          driveClient.appendNamedRange(
-            noteTextArea.val(),
-            sefariaRef.amud,
-            sefariaRef.ref,
-            sefariaRef.parentRef || sefariaRef.ref);
-          modalContainer.hide();
-        });
-      },
-    });
-  }
-
-  snackbars.textSelection.show(ref, buttons);
-});
 
 $(document).ready(main);
+
+onceDocumentReady.execute(registerRefSelectionSnackbarListener);
 
 onceDocumentReady.execute(() => {
   const modalContainer = $("#modal-container");
