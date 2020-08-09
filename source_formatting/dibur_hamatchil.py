878108d121cae99cac5f869bcfbd3e75f4473969
Ron Shapiro <shapiro.rd@gmail.com>
2020-08-09 21:25:32 +0300

    Detect emdashes as diburei hamatchil splitters in addition to regular dashes

--- source_formatting/dibur_hamatchil.py
+++ source_formatting/dibur_hamatchil.py
@@ -6,12 +6,18 @@ _TITLES_TO_BOLDIFY = (
     "Tosafot",
 )
 
+_SPLITTERS = (
+    " - ",
+    " – ",
+)
+
 def bold_diburei_hamatchil(text, commentary_name):
     if commentary_name not in _TITLES_TO_BOLDIFY:
         return text
 
-    if " - " not in text:
-        return text
+    for splitter in _SPLITTERS:
+        if splitter in text:
+            dibur_hamatchil, comment = text.split(splitter, 1)
+            return f'<strong class="dibur-hamatchil">{dibur_hamatchil}</strong>{splitter}{comment}'
 
-    dibur_hamatchil, comment = text.split(" - ", 1)
-    return f'<strong class="dibur-hamatchil">{dibur_hamatchil}</strong> - {comment}'
+    return text
