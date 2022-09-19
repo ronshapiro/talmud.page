fe2f31f12e091803bf0cadaa5b3cf925030bca42
Ron Shapiro <shapiro.rd@gmail.com>
2022-09-19 03:31:50 +0300

    Remove logs in API tests

--- __tests__/api_request_handler.test.ts
+++ __tests__/api_request_handler.test.ts
@@ -9,10 +9,11 @@ import * as fs from "fs";
 import {jsonStringify} from "../util/json_stringify";
 import {ApiRequestHandler} from "../api_request_handler";
 import {FakeRequestMaker, testPages} from "./api_request_handler_base";
+import {NoopLogger} from "../logger";
 
 const handler = new ApiRequestHandler(new FakeRequestMaker());
 test.each(testPages)("%s", testPage => {
-  return handler.handleRequest(testPage.title, testPage.page)
+  return handler.handleRequest(testPage.title, testPage.page, new NoopLogger())
     .then(results => {
       const expected = fs.readFileSync(testPage.outputFilePath(), {encoding: "utf-8"});
       expect(jsonStringify(results)).toBe(expected);
