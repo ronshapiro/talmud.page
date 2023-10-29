860733442ada37e8cc9226aae3f5b51bd2930e39
Ron Shapiro <shapiro.rd@gmail.com>
2023-10-29 12:22:56 +0200

    Turn off more eslint options in dev

--- .eslintrc.dev.js
+++ .eslintrc.dev.js
@@ -1,9 +1,23 @@
 /* eslint-disable quote-props */
 
+const TURN_OFF = {};
+
+for (const key of [
+  "comma-spacing",
+  "key-spacing",
+  "space-infix-ops",
+  "no-console",
+  "semi",
+  "semi-spacing",
+  "spaced-comment",
+  "@typescript-eslint/no-extra-semi",
+]) {
+  TURN_OFF[key] = "off";
+}
+
 module.exports = {
   extends: [".eslintrc.js"],
   rules: {
-    "no-console": "off",
-    "spaced-comment": "off",
+    ...TURN_OFF,
   },
 };
