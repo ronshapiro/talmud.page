d292fe2c34092cecc3e3fc375d81d2e4d10dccd4
Ron Shapiro <shapiro.rd@gmail.com>
2024-10-14 14:31:07 +0300

    Detect gsed existence

--- pre-commit/node_version.sh
+++ pre-commit/node_version.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-NODE_MAJOR_VERSION="$(node --version | gsed "s/v\([[:digit:]]\+\).*/\1/")"
+if which gsed &> /dev/null; then
+    SED=gsed
+else
+    SED=sed
+fi
+
+NODE_MAJOR_VERSION="$(node --version | ${SED} "s/v\([[:digit:]]\+\).*/\1/")"
 if [[ "${NODE_MAJOR_VERSION}" == "21" ]]; then
     NODE_MAJOR_VERSION=20
 fi
 
 
-gsed -i "s/runtime: nodejs.*/runtime: nodejs${NODE_MAJOR_VERSION}/" app.yaml
+"${SED}" -i "s/runtime: nodejs.*/runtime: nodejs${NODE_MAJOR_VERSION}/" app.yaml
