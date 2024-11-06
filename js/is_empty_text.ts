fc8d9d750c0078634ce32bd7c9b9a4c43932239c
Ron Shapiro <shapiro.rd@gmail.com>
2024-11-06 15:37:42 +0200

    Fix isEmptyText to detect undefined == empty

--- js/is_empty_text.ts
+++ js/is_empty_text.ts
@@ -3,7 +3,7 @@ import * as React from "react";
 export default function isEmptyText(
   stringOrList: string | string[] | React.ReactElement | undefined): boolean {
   if (stringOrList === undefined) {
-    return false;
+    return true;
   }
   return !stringOrList || stringOrList === "" || (
     typeof stringOrList === "string" && stringOrList.length === 0);
