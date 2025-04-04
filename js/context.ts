f5e7d8233e3064f01e3bf1ed85cfe2ced874e903
Ron Shapiro <shapiro.rd@gmail.com>
2025-04-04 16:59:34 +0300

    Fix usages of basic contexts for preferences that are not in a renderer context. This is a quick hack to fix the homepage and browse page

--- js/context.ts
+++ js/context.ts
@@ -2,9 +2,14 @@ import * as React from "react";
 
 const {createContext, useContext} = React;
 
+const DEFAULT_CONTEXT = {
+  versions: () => [],
+  rendererType: "default",
+};
+
 export const ConfigurationContext = createContext<any>(undefined);
 export function useConfiguration(): any {
-  return useContext(ConfigurationContext);
+  return useContext(ConfigurationContext) ?? DEFAULT_CONTEXT;
 }
 
 export const HiddenHostContext = createContext<any>(undefined);
