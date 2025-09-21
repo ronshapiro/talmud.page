97d6270a99dc8c5914459439d8e0b5cbc82c9c62
Ron Shapiro <shapiro.rd@gmail.com>
2025-09-21 11:37:19 +0300

    Revert "Debug highlighting disappearance"
    This reverts commit 7dc06bbd696ef4ee05054f85bb2dbbb1927eea61.

--- js/ref_selection_snackbar.ts
+++ js/ref_selection_snackbar.ts
@@ -11,11 +11,10 @@ import {Button, snackbars} from "./snackbar";
 import {checkNotUndefined} from "./undefined";
 
 let selectionSnackbarRef: string | undefined;
-const hideSelectionChangeSnackbar = (arg: string) => {
+const hideSelectionChangeSnackbar = () => {
   if (selectionSnackbarRef) {
     selectionSnackbarRef = undefined;
-    // snackbars.textSelection.hide();
-    snackbars.textSelection.show(arg, []);
+    snackbars.textSelection.hide();
   }
 };
 
@@ -91,25 +90,17 @@ const findSefariaRefOrHideSnackbar = (): FindSefariaRefReturnType => {
   }
 
   if (selection.type !== "Range") {
-    hideSelectionChangeSnackbar(selection.type);
+    hideSelectionChangeSnackbar();
     return undefined;
   }
 
   const sefariaRef = findSefariaRef(selection.anchorNode);
-  if (sefariaRef === undefined) {
-    hideSelectionChangeSnackbar("undefined");
-    return undefined;
-  }
-  if (!sefariaRef.ref) {
-    hideSelectionChangeSnackbar("empty ref");
-    return undefined;
-  }
   if (sefariaRef === undefined
       || !sefariaRef.ref
       // TODO: perhaps support multiple refs, and just grab everything in between?
       // If the selection spans multiple refs, ignore them all
       || sefariaRef.ref !== findSefariaRef(selection.focusNode)?.ref) {
-    hideSelectionChangeSnackbar("unknown");
+    hideSelectionChangeSnackbar();
     return undefined;
   }
   if (sefariaRef.ref === selectionSnackbarRef) {
