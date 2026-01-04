c8961a9e829ae5a146d2c416fabf01a67d88af99
Ron Shapiro <shapiro.rd@gmail.com>
2026-01-04 11:21:57 +0200

    Dedupe developer emails in mailjet

--- mailjet.ts
+++ mailjet.ts
@@ -1,3 +1,4 @@
+import * as fs from "fs";
 import * as Mailjet from "node-mailjet";
 import {readUtf8} from "./files";
 
@@ -26,13 +27,18 @@ export async function sendEmail({to, cc, subject, text, html}: SendParameters): 
     apiSecret: readUtf8("mailjet_api_secret"),
   });
 
+
+  const ignoreAddresses = fs.existsSync("ignore_email_addresses")
+    ? new Set(readUtf8("ignore_email_addresses").split(","))
+    : new Set<string>();
+
   return client.post('send', { version: 'v3.1' })
     .request({
       Messages: [
         {
           From: makeEmail("mail@talmud.page", "talmud.page"),
           To: [makeEmail(to)],
-          CC: (cc ?? []).map(x => makeEmail(x)),
+          CC: (cc ?? []).filter(x => !ignoreAddresses.has(x)).map(x => makeEmail(x)),
           Subject: subject,
           TextPart: text,
           HTMLPart: html,
